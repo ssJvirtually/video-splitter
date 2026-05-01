0856d59510c07eb0693512fc2259007063664f97
Shashi Jakka <jskr456@gmail.com>
2026-05-01 20:15:54 +0530

    Add GitHub Pages deployment config and COI service worker for FFmpeg

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/video-splitter/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
