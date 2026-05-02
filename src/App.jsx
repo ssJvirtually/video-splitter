c6a2affb673fa05a2a787a3ef827e353e9701c41
Shashi Jakka <jskr456@gmail.com>
2026-05-02 12:48:24 +0530

    Implement clean URLs with BrowserRouter and Cloudflare redirection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import VideoSplitter from './pages/VideoSplitter'
 import './style.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/splitter">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/splitter" element={<VideoSplitter />} />
