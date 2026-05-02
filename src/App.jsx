6449eef9235991e4746f06493d137933ba35e751
Shashi Jakka <jskr456@gmail.com>
2026-05-02 12:50:10 +0530

    Final Fix: Use HashRouter and relative base for universal GH Pages and Local support

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import VideoSplitter from './pages/VideoSplitter'
 import './style.css'
 
 function App() {
   return (
-    <Router basename="/splitter">
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/splitter" element={<VideoSplitter />} />
